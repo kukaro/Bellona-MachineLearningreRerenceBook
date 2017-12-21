b70c88655abaa5dfd9a48bd17cf621412f858211
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-22 08:31:22 +0900

    add double session

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import reference_book.a_base_graph.first_tensorflow as a1
 import reference_book.a_base_graph.second_reduce as a2
 import reference_book.a_base_graph.third_feed_dict as a3
 import reference_book.b_tensorboard.test_tensorboard as b
-import reference_book.c_reference_test.fill_atom as c
+import reference_book.c_reference_test.fill_atom as c1
+import reference_book.c_reference_test.double_session as c2
 
 if '__main__':
-    c.run()
+    c2.run()
---
+++ reference_book/c_reference_test/double_session.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+def run():
+    a = tf.Variable(0)
+    init = tf.initialize_all_variables()
+
+    sess1 = tf.Session()
+    sess2 = tf.Session()
+
+    sess1.run(init)
+    sess2.run(init)
+
+    print(sess1.run(a.assign_add(5)))
+    print(sess2.run(a.assign_add(7)))
+    print(sess1.run(a.assign_add(5)))
+    print(sess2.run(a.assign_add(7)))
+
+
+    '''
+    변수 별로 관리되는게 아니라 세션별로 관리된다.
+    '''
