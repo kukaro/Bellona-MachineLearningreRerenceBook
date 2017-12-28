11481eda5cf75483531fdcaf33227f6ca2fa1fbb
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-29 02:44:42 +0900

    add dimension

---
+++ reference_book/h_cnn/example.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+image_batch = tf.constant([
+    [  # First Image
+        [[0, 255, 0], [0, 255, 0], [0, 255, 0]],
+        [[0, 255, 0], [0, 255, 0], [0, 255, 0]]
+    ],
+    [  # Second Image
+        [[0, 0, 255], [0, 0, 255], [0, 0, 255]],
+        [[0, 0, 255], [0, 0, 255], [0, 0, 255]]
+    ]
+])
+image_batch.get_shape()
+sess = tf.Session()
+print(sess.run(image_batch))
+print(image_batch.get_shape())
