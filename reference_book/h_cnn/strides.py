cb31f02570eb460a9f99f408d63b7b2bf0efe550
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-31 17:31:49 +0900

    add strides

---
+++ reference_book/h_cnn/strides.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+# 0.0부터 5.5까지를 0.1간격으로 출력
+input_batch = tf.constant([  # first input (6*6*1)
+    [[[float(x + y / 10)] for x in range(0, 6)] for y in range(0, 6)]
+])
+
+kernel = tf.constant([  # kernel (3*3*1)
+    [[[0.0]], [[0.5]], [[0.0]]],
+    [[[0.0]], [[1.0]], [[0.0]]],
+    [[[0.0]], [[0.5]], [[0.0]]]
+])
+
+strides = [1, 3, 3, 1]
+
+conv2d = tf.nn.conv2d(input_batch, kernel, strides=strides, padding='SAME')
+
+sess = tf.Session()
+print(sess.run(conv2d))
+# print(sess.run(input_batch))
