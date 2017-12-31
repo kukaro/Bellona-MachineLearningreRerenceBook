7017218476b66717bf8a5737a978164dd12eb8aa
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-31 21:28:10 +0900

    이미지는 도중에 sess.run해도 출력되지 않고 익셉션 된다. 이런 예제는 파일관리하는데서 몇개 볼수있는것 같다. 엣지잇게 배우는 텐서플로우 165장에 kernels in depth부분

--- reference_book/h_cnn/kernels_in_depth.py
+++ reference_book/h_cnn/kernels_in_depth.py
@@ -5,7 +5,7 @@ image_filename = 'image.jpg'
 filename_queue = tf.train.string_input_producer(
     tf.train.match_filenames_once([os.path.dirname(__file__) + '/' + image_filename]))
 image_reader = tf.WholeFileReader()
-_,image_file = image_reader.read(filename_queue)
+_, image_file = image_reader.read(filename_queue)
 image = tf.image.decode_jpeg(image_file)
 
 kernel = tf.constant([
@@ -29,9 +29,17 @@ with tf.Session() as sess:
     sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
-    print(sess.run(image_file))
+    img_f = tf.constant(sess.run(image), dtype=tf.float32)
     coord.request_stop()
     coord.join(threads)
-    sess.run(image)
-    conv2d = tf.nn.conv2d(image, kernel, strides=[1, 1, 1, 1], padding='SAME')
-# activation_map = sess.run(tf.minimum(tf.nn.relu(conv2d), 255))
+    result = sess.run(img_f)
+    print(len(result))
+    for i in range(len(result)):
+        print('#%d' % i)
+        print(result[i])
+    conv2d = tf.nn.conv2d([img_f], kernel, strides=[1, 1, 1, 1], padding='SAME')
+    activation_map = sess.run(tf.minimum(tf.nn.relu(conv2d), 255))
+    result_conv2 = sess.run(conv2d)
+    for i in range(len(result_conv2[0])):
+        print('#%d' % i)
+        print(result_conv2[0][i])
