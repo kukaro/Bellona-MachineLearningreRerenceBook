5c708e4cc22184b7399c638eda06fcad78929d33
KTJ <justkukaro@naver.com>
2018-03-18 19:15:12 +0900

    mnist 예제

---
+++ reference_book_scratch/b_learning/mnist_test.py
@@ -0,0 +1,11 @@
+import sys, os
+
+sys.path.append(os.pardir)
+from reference_book_scratch.b_learning.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
