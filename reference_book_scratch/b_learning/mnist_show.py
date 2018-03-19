c1e2db56fbc648de187f9355369c5da22d090c06
KTJ <justkukaro@naver.com>
2018-03-19 13:10:46 +0900

    라이브러리 추가

---
+++ reference_book_scratch/b_learning/mnist_show.py
@@ -0,0 +1,26 @@
+from reference_book_scratch.b_learning.mnist import load_mnist
+from PIL import Image
+
+import sys
+import os
+import numpy as np
+
+sys.path.append(os.pardir)
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_test[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
